8f2a4d2b64e460cf7f3ecfc70f27c044737f5792
Markus Johansson <8209majo.uddevalla.se>
2022-04-07 16:11:02 +0200

    Add project files.

---
+++ CoolBooks2.0/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using CoolBooks.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoolBooks.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Books()
+        {
+            return View();
+        }
+        public IActionResult Reviews()
+        {
+            return View();
+        }
+
+        public IActionResult Authors()
+        {
+            return View();
+        }
+
+        public IActionResult Genres()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoolBooks2.0/README.md
@@ -0,0 +1,12 @@
+# CoolBooks
+Slutprojekt för Bunny cherifs
+Hej alla glada människor!
+
+
+-Insert sunglasses emoji-
+
+-Funkar-
+
+Test ifrån Visual studio - Gabriel
+
+Test ifrån Visual studio - Markus
