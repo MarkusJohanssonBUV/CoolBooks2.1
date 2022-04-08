b3e3a3d4b0a7a62a437cb50533b14f8dc111d9cf
Markus Johansson <8209majo.uddevalla.se>
2022-04-08 09:33:40 +0200

    ChangedDatabaseRelationstabell

--- CoolBooks2.0/Data/CoolbooksContext.cs
+++ CoolBooks2.0/Data/CoolbooksContext.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using CoolBooks.Models;
 
-namespace CoolBooks.Data 
+namespace CoolBooks.Data
 {
     public partial class CoolbooksContext : DbContext
     {
@@ -21,6 +21,7 @@ namespace CoolBooks.Data
 
         public virtual DbSet<Authors> Authors { get; set; }
         public virtual DbSet<Books> Books { get; set; }
+        public virtual DbSet<BooksUsers> BooksUsers { get; set; }
         public virtual DbSet<Genres> Genres { get; set; }
         public virtual DbSet<Reviews> Reviews { get; set; }
         public virtual DbSet<UserInfo> UserInfo { get; set; }
@@ -30,7 +31,7 @@ namespace CoolBooks.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=MARKUS\\MARKUSSQLEXPRESS;Initial Catalog=CoolBooks;Integrated Security=True");
+                optionsBuilder.UseSqlServer("Data Source=MARKUS\\MARKUSSQLEXPRESS;Initial Catalog=Coolbooks4;Integrated Security=True");
             }
         }
 
@@ -57,25 +58,42 @@ namespace CoolBooks.Data
                     .HasConstraintName("Fk_Genres");
             });
 
+            modelBuilder.Entity<BooksUsers>(entity =>
+            {
+                entity.HasKey(e => new { e.BooksID, e.UserID });
+
+                entity.HasOne(d => d.Books)
+                    .WithMany(p => p.BooksUsers)
+                    .HasForeignKey(d => d.BooksID)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_BooksUsers_Books");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.BooksUsers)
+                    .HasForeignKey(d => d.UserID)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_BooksUsers_UserID");
+            });
+
             modelBuilder.Entity<Genres>(entity =>
             {
                 entity.HasKey(e => e.GenerID)
                     .HasName("Pk_GenerID");
             });
 
-            modelBuilder.Entity<UserInfo>(entity =>
+            modelBuilder.Entity<Reviews>(entity =>
             {
-                entity.HasOne(d => d.Books)
-                    .WithMany(p => p.UserInfo)
-                    .HasForeignKey(d => d.BooksID)
+                entity.HasOne(d => d.Book)
+                    .WithMany(p => p.Reviews)
+                    .HasForeignKey(d => d.BookID)
                     .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("Fk_User_Books");
+                    .HasConstraintName("Fk_Books_Reviews");
 
-                entity.HasOne(d => d.Reviews)
-                    .WithMany(p => p.UserInfo)
-                    .HasForeignKey(d => d.ReviewsID)
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Reviews)
+                    .HasForeignKey(d => d.UserID)
                     .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("Fk_UserID");
+                    .HasConstraintName("Fk_User_Reviews");
             });
 
             OnModelCreatingPartial(modelBuilder);
