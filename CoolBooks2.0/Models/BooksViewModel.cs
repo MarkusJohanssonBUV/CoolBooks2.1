1b170d30b8bc3079ca960bc95aa355512998926d
Markus Johansson <8209majo.uddevalla.se>
2022-04-22 15:23:12 +0200

    MarkusFredag

--- CoolBooks2.0/Models/BooksViewModel.cs
+++ CoolBooks2.0/Models/BooksViewModel.cs
@@ -1,21 +1,31 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CoolBooks.Models
 {
     [NotMapped]
     public partial class BooksViewModel
     {
+
+        //OBS Kolla på att göra om denna så att den innhåller listor av Books, Authors osv, använd MVC-Movie som exempel!!
         public string Title { get; set; }
         public string Description { get; set; }
         public string ISBN { get; set; }
         public string ImagePath { get; set; }
 
+        public bool IsDeleted { get; set; }
         public int BooksID { get; set; }
         //public string AuthorName { get; set; }
         //public string GenreName { get; set; }
 
+        public List<Books>? Books { get; set; }
+
         public List<string> AuthorName { get; set; }
         public List<string> GenreName { get; set; }
+        public List<string> AllGenreName { get; set; }
+
+        public SelectList AuthorNameSelect { get; set; }
+        public SelectList GenreNameSelect { get; set; }
         public List<string> UserName { get; set; }
 
         public DateTime? Created { get; set; }
