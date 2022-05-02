aec15c29be576a3a977efce7228f5ea24af2b117
andreaskbostrom <andreaskbostrom@gmail.com>
2022-05-02 09:22:34 +0200

    AndreasMåndag

---
+++ CoolBooks2.0/Models/DataPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace CoolBooks.Models
+{
+	//DataContract for Serializing Data - required to serve in JSON format
+	[DataContract]
+	public class DataPoint
+	{
+		//public DataPoint(List<string> label, int y)
+		//{
+		//	this.Label = label;
+		//	this.Y = y;
+		//}
+
+		//Explicitly setting the name to be used while serializing to JSON.
+		[DataMember(Name = "label")]
+		public string Label { get; set; }
+
+		//Explicitly setting the name to be used while serializing to JSON.
+		[DataMember(Name = "y")]
+		public Nullable<int> Y {get;set;}
+	}
+}
