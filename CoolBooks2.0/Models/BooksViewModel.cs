8d198fd82704a3506116c2dbea9ecbf76307d8d4
fClueless <103027959+fClueless@users.noreply.github.com>
2022-05-03 13:56:36 +0200

    Sista ändringarna, mest css

--- CoolBooks2.0/Models/BooksViewModel.cs
+++ CoolBooks2.0/Models/BooksViewModel.cs
@@ -31,10 +31,11 @@ namespace CoolBooks.Models
         public List<string> AllGenreName { get; set; }
 
         public List<Reviews>? Reviews { get; set; }
-        public List<string> ReviewText { get; set; }
-        public List<string> ReviewTitle { get; set; }
-        public List<string> ReviewRating { get; set; }
-        public List<string> ReviewBookId { get; set; }
+        public string ReviewText { get; set; }
+        public string ReviewTitle { get; set; }
+        public string ReviewRating { get; set; }
+        public string ReviewBookId { get; set; }
+        public string ClientId { get; set; }    
         public List<int> RandomBooklist { get; set; }
 
 
